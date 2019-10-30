137cdecc895945c404726395afb5cf053f707db5
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-10-30 17:47:48 -0300

    Remove textarea warning

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './input-text.module.css';
 
-const InputText = props => (
-  <form className={styles.form} onSubmit={props.onSendText}>
-    <textarea className={styles.text} name="text">
-      {props.usernameToReply ? `@${props.usernameToReply} ` : ''}
-    </textarea>
-    <div className={styles.buttons}>
-      <button className={styles.close} onClick={props.onCloseText}>
-        Cerrar
-      </button>
-      <button className={styles.send} type="submit">
-        Enviar
-      </button>
-    </div>
-  </form>
-);
+const InputText = props => {
+  const username = props.usernameToReply ? `@${props.usernameToReply} ` : '';
+  return (
+    <form className={styles.form} onSubmit={props.onSendText}>
+      <textarea className={styles.text} name="text" defaultValue={username} />
+      <div className={styles.buttons}>
+        <button className={styles.close} onClick={props.onCloseText}>
+          Cerrar
+        </button>
+        <button className={styles.send} type="submit">
+          Enviar
+        </button>
+      </div>
+    </form>
+  );
+};
 
 InputText.propTypes = {
   usernameToReply: PropTypes.string,
